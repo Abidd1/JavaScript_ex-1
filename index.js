06999aca6c249838f1e6b7b098e52a85d44f2fb8
Abid Ali <abidali129418@gmail.com>
2025-01-19 14:03:53 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,87 @@
+// Task First 
+
+alert("Hello, World!");
+
+
+// Task second 
+
+let firstName = "Abid";  
+let lastName = "Ali"; 
+let fullName = firstName + " " + lastName;
+alert(fullName);
+
+// Task Three 
+
+let num1 = 10;
+let num2 = 5;
+
+alert("Sum: " + (num1 + num2));
+alert("Difference: " + (num1 - num2));
+alert("Product: " + (num1 * num2));
+alert("Division: " + (num1 / num2));
+alert("Modulus: " + (num1 % num2));
+
+
+// Task four 
+
+let width = 10;
+let height = 5;
+let area = width * height;
+alert("Area: " + area);
+
+// Task Five 
+
+let count = 10;
+
+alert("Initial count: " + count);
+
+alert("Post-increment: " + count++); 
+alert("Count after post-increment: " + count); 
+
+alert("Pre-increment: " + ++count); 
+alert("Count after pre-increment: " + count); 
+
+alert("Post-decrement: " + count--); 
+alert("Count after post-decrement: " + count); 
+
+alert("Pre-decrement: " + --count); 
+alert("Count after pre-decrement: " + count); 
+
+
+// Task six 
+
+let count = 10;
+
+alert("Initial count: " + count);
+
+alert("Post-increment: " + count++); 
+alert("Count after post-increment: " + count); 
+
+alert("Pre-increment: " + ++count); 
+alert("Count after pre-increment: " + count); 
+
+alert("Post-decrement: " + count--); 
+alert("Count after post-decrement: " + count); 
+
+alert("Pre-decrement: " + --count); 
+alert("Count after pre-decrement: " + count); 
+
+
+// Task Seven 
+
+
+let result = ((15 - 3 * 2) / 3 + 4**2) * 2 - (8 / 2)**2;
+alert("Result: " + result); 
+
+let resultCorrect = (((15 - 3 * 2) / 3) + (4**2)) * 2 - (8 / 2)**2;
+alert("Corrected Result: " + resultCorrect);
+
+
+
+// Task Eight 
+
+
+let name = "YourName"; 
+let age = 30; 
+let message = "Hello, " + name + "! You are " + age + " years old.";
+console.log(message);
